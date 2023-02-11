18ccda0a95b145c9b5aff36d27bbb6e7de8de391
Cassin01 <koyojb@gmail.com>
2023-02-11 14:23:31 +0900

    update: ../README.md../lua/wf/builtin/bookmark.lua../lua/wf/builtin/buffer.lua../lua/wf/builtin/register.lua../lua/wf/builtin/which_key.lua../lua/wf/cell.lua../lua/wf/common.lua../lua/wf/config/init.lua../lua/wf/config/style.lua../lua/wf/core.lua../lua/wf/fuzzy.lua../lua/wf/group.lua../lua/wf/init.lua../lua/wf/input.lua../lua/wf/output.lua../lua/wf/output_obj_which.lua../lua/wf/prompt_counter.lua../lua/wf/setup/init.lua../lua/wf/skip_front_duplication.lua../lua/wf/sorter/init.lua../lua/wf/static.lua../lua/wf/util.lua../lua/wf/which.lua../lua/wf/which_map.lua../plugin/wf.lua../scripts/minimal_init.luahelpers.luatest_API.lua

--- lua/wf/builtin/bookmark.lua
+++ lua/wf/builtin/bookmark.lua
@@ -2,79 +2,79 @@ local select = require("wf").select
 local gen_highlight = require("wf.util").gen_highlight
 
 local function require_deviocon()
-    return require("nvim-web-devicons")
+  return require("nvim-web-devicons")
 end
 
 local ok, devicon = pcall(require_deviocon)
 
 local function bookmark(bookmark_dirs, opts)
-    local function _bookmark()
-        opts = opts or {}
-        local _opts = {
-            title = "Bookmark",
-            behavior = {
-                skip_front_duplication = true,
-                skip_back_duplication = true,
-            },
-            output_obj_which_mode_desc_format = function(match_obj)
-                local desc = match_obj.text
-                if ok then
-                    if match_obj.type == "group" then
-                        return { { desc, "WFWhichDesc" }, { " +", "WFExpandable" } }
-                    else
-                        local icon, color = devicon.get_icon_color(desc)
-                        if icon ~= nil then
-                            local name = gen_highlight(desc, color)
-                            local sp = vim.fn.strwidth(icon) > 1 and (icon .. "") or (icon .. " ")
-                            return { { sp .. " ", name }, { desc, "WFWhichDesc" } }
-                        else
-                            return { { "  ", "Directory" }, { desc, "WFWhichDesc" } }
-                        end
-                    end
-                else
-                    if match_obj.type == "group" then
-                        return { { desc, "WFWhichDesc" }, { " +", "WFExpandable" } }
-                    else
-                        return { { desc, "WFWhichDesc" } }
-                    end
-                end
-            end,
-        }
-        for k, v in pairs(opts) do
-            _opts[k] = v
-        end
-
-        select(bookmark_dirs, _opts, function(path_, _)
-            local path = vim.fn.expand(path_)
-            if vim.fn.isdirectory(path) ~= 0 then
-                if vim.fn.exists(":Telescope") then
-                    require("telescope").extensions.file_browser.file_browser({
-                        path = path_,
-                        depth = 4,
-                    })
-                    return
-                elseif vim.fn.exists(":CtrlP") then
-                    local command = "CtrlP " .. path
-                    vim.cmd(command)
-                    return
-                elseif vim.g.loaded_netrwPlugin == 0 and vim.g.loaded_netrw == 0 then
-                    local command = "e " .. path
-                    vim.cmd(command)
-                    return
-                else
-                    print("not matched")
-                end
-            elseif vim.fn.filereadable(path) ~= 0 then
-                local command = "vi " .. path
-                vim.cmd(command)
-                return
+  local function _bookmark()
+    opts = opts or {}
+    local _opts = {
+      title = "Bookmark",
+      behavior = {
+        skip_front_duplication = true,
+        skip_back_duplication = true,
+      },
+      output_obj_which_mode_desc_format = function(match_obj)
+        local desc = match_obj.text
+        if ok then
+          if match_obj.type == "group" then
+            return { { desc, "WFWhichDesc" }, { " +", "WFExpandable" } }
+          else
+            local icon, color = devicon.get_icon_color(desc)
+            if icon ~= nil then
+              local name = gen_highlight(desc, color)
+              local sp = vim.fn.strwidth(icon) > 1 and (icon .. "") or (icon .. " ")
+              return { { sp .. " ", name }, { desc, "WFWhichDesc" } }
             else
-                print("The file/dir does not found")
+              return { { "  ", "Directory" }, { desc, "WFWhichDesc" } }
             end
-        end)
+          end
+        else
+          if match_obj.type == "group" then
+            return { { desc, "WFWhichDesc" }, { " +", "WFExpandable" } }
+          else
+            return { { desc, "WFWhichDesc" } }
+          end
+        end
+      end,
+    }
+    for k, v in pairs(opts) do
+      _opts[k] = v
     end
 
-    return _bookmark
+    select(bookmark_dirs, _opts, function(path_, _)
+      local path = vim.fn.expand(path_)
+      if vim.fn.isdirectory(path) ~= 0 then
+        if vim.fn.exists(":Telescope") then
+          require("telescope").extensions.file_browser.file_browser({
+            path = path_,
+            depth = 4,
+          })
+          return
+        elseif vim.fn.exists(":CtrlP") then
+          local command = "CtrlP " .. path
+          vim.cmd(command)
+          return
+        elseif vim.g.loaded_netrwPlugin == 0 and vim.g.loaded_netrw == 0 then
+          local command = "e " .. path
+          vim.cmd(command)
+          return
+        else
+          print("not matched")
+        end
+      elseif vim.fn.filereadable(path) ~= 0 then
+        local command = "vi " .. path
+        vim.cmd(command)
+        return
+      else
+        print("The file/dir does not found")
+      end
+    end)
+  end
+
+  return _bookmark
 end
 
 return bookmark
--- tests/test_API.lua
+++ tests/test_API.lua
@@ -4,63 +4,60 @@ local helpers = dofile("tests/helpers.lua")
 
 local child = helpers.new_child_neovim()
 local eq_global, eq_config, eq_state =
-    helpers.expect.global_equality, helpers.expect.config_equality, helpers.expect.state_equality
+	helpers.expect.global_equality, helpers.expect.config_equality, helpers.expect.state_equality
 local eq_type_global, eq_type_config, eq_type_state =
-    helpers.expect.global_type_equality,
-    helpers.expect.config_type_equality,
-    helpers.expect.state_type_equality
+	helpers.expect.global_type_equality, helpers.expect.config_type_equality, helpers.expect.state_type_equality
 
 local T = MiniTest.new_set({
-    hooks = {
-        -- This will be executed before every (even nested) case
-        pre_case = function()
-            -- Restart child process with custom 'init.lua' script
-            child.restart({ "-u", "scripts/minimal_init.lua" })
-        end,
-        -- This will be executed one after all tests from this set are finished
-        post_once = child.stop,
-    },
+	hooks = {
+		-- This will be executed before every (even nested) case
+		pre_case = function()
+			-- Restart child process with custom 'init.lua' script
+			child.restart({ "-u", "scripts/minimal_init.lua" })
+		end,
+		-- This will be executed one after all tests from this set are finished
+		post_once = child.stop,
+	},
 })
 
 -- Tests related to the `setup` method.
 T["setup()"] = MiniTest.new_set()
 
 T["setup()"]["sets exposed methods and default options value"] = function()
-    child.lua([[require('wf').setup()]])
+	child.lua([[require('wf').setup()]])
 
-    -- -- global object that holds your plugin information
-    -- eq_type_global(child, "_G.Wf", "table")
+	-- -- global object that holds your plugin information
+	-- eq_type_global(child, "_G.Wf", "table")
 
-    -- -- public methods
-    -- eq_type_global(child, "_G.Wf.toggle", "function")
-    -- eq_type_global(child, "_G.Wf.disable", "function")
-    -- eq_type_global(child, "_G.Wf.enable", "function")
+	-- -- public methods
+	-- eq_type_global(child, "_G.Wf.toggle", "function")
+	-- eq_type_global(child, "_G.Wf.disable", "function")
+	-- eq_type_global(child, "_G.Wf.enable", "function")
 
-    -- -- config
-    -- eq_type_global(child, "_G.Wf.config", "table")
+	-- -- config
+	-- eq_type_global(child, "_G.Wf.config", "table")
 
-    -- -- assert the value, and the type
-    -- eq_config(child, "debug", false)
-    -- eq_type_config(child, "debug", "boolean")
+	-- -- assert the value, and the type
+	-- eq_config(child, "debug", false)
+	-- eq_type_config(child, "debug", "boolean")
 end
 
 T["setup()"]["overrides default values"] = function()
-    child.lua([[require('wf').setup({
+	child.lua([[require('wf').setup({
         -- write all the options with a value different than the default ones
         theme = "chad",
     })]])
-    eq_config(child, "theme", "chad")
-    eq_type_config(child, "theme", "string")
+	eq_config(child, "theme", "chad")
+	eq_type_config(child, "theme", "string")
 
+	-- child.lua([[require('wf').setup({
+	--     -- write all the options with a value different than the default ones
+	--     debug = true,
+	-- })]])
 
-    -- child.lua([[require('wf').setup({
-    --     -- write all the options with a value different than the default ones
-    --     debug = true,
-    -- })]])
-
-    -- -- assert the value, and the type
-    -- eq_config(child, "debug", true)
-    -- eq_type_config(child, "debug", "boolean")
+	-- -- assert the value, and the type
+	-- eq_config(child, "debug", true)
+	-- eq_type_config(child, "debug", "boolean")
 end
 
 return T
