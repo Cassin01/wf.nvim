4110322ce6a677cd088e576e5e280de8bff83826
Cassin01 <koyojb@gmail.com>
2023-02-28 19:14:20 +0900

    update: init.lua,

---
+++ lua/wf/types.lua
@@ -0,0 +1,28 @@
+---@alias WFTheme "default"|"space"|"chad"
+
+---@class WFHighlight
+---@field WFNormal string
+---@field WFNormal string
+---@field WFFloatBorder string
+---@field WFFloatBorderFocus string
+---@field WFComment string
+---@field WFWhichRem string
+---@field WFWhichOn string
+---@field WFFuzzy string
+---@field WFFuzzyPrompt string
+---@field WFFocus string
+---@field WFFreeze string
+---@field WFWhichObjCounter string
+---@field WFWhichDesc string
+---@field WFSeparator string
+---@field WFGroup string
+---@field WFWhichUnique string
+---@field WFExpandable string
+---@field WFTitleOutput string
+---@field WFTitleWhich string
+---@field WFTitleFuzzy string
+---@field WFTitleFreeze string
+
+---@class WFConfig
+---@field theme? WFTheme
+---@field highlight? WFHighlight
