462b0c38c2e48e18d09f5d564ec89e2ef541403c
Cassin01 <koyojb@gmail.com>
2023-03-03 10:07:28 +0900

    update: wf.txt, types.lua,

--- lua/wf/types.lua
+++ lua/wf/types.lua
@@ -1,29 +1,65 @@
 ---@alias WFTheme "default"|"space"|"chad"
 
+---@class hl_group string Highlight group name, e.g. "ErrorMsg"
+
 ---@class WFHighlight
----@field WFNormal string
----@field WFFloatBorder string
----@field WFFloatBorderFocus string
----@field WFComment string
----@field WFWhichRem string
----@field WFWhichOn string
----@field WFFuzzy string
----@field WFFuzzyPrompt string
----@field WFFocus string
----@field WFFreeze string
----@field WFWhichObjCounter string
----@field WFWhichDesc string
----@field WFSeparator string
----@field WFGroup string
----@field WFWhichUnique string
----@field WFExpandable string
----@field WFTitleOutput string
----@field WFTitleWhich string
----@field WFTitleFuzzy string
----@field WFTitleFreeze string
+---@field WFNormal hl_group
+---@field WFFloatBorder hl_group
+---@field WFFloatBorderFocus hl_group
+---@field WFComment hl_group
+---@field WFWhichRem hl_group
+---@field WFWhichOn hl_group
+---@field WFFuzzy hl_group
+---@field WFFuzzyPrompt hl_group
+---@field WFFocus hl_group
+---@field WFFreeze hl_group
+---@field WFWhichObjCounter hl_group
+---@field WFWhichDesc hl_group
+---@field WFSeparator hl_group
+---@field WFGroup hl_group
+---@field WFWhichUnique hl_group
+---@field WFExpandable hl_group
+---@field WFTitleOutput hl_group
+---@field WFTitleWhich hl_group
+---@field WFTitleFuzzy hl_group
+---@field WFTitleFreeze hl_group
 
 ---@class WFConfig
 ---@field theme? WFTheme
 ---@field highlight? WFHighlight
 
+---@class WFBehavior
+---@field skip_front_duplication boolean
+---@field skip_back_duplication boolean
+
+---@class Cell
+---@field key string
+---@field id string
+---@field text string
+---@field type "key"|"group"
+
+---@class Chunks
+---A list of [text, hl_group] arrays, each representing a text chunk with specified highlight. hl_group element can not be omitted for not
+--highlight.
+
+---@class WFStyle
+---@field border string
+---Style of window border. This can either be a string or an array.
+---:h nvim_open_win
+---@field borderchars table @field top center bottom 
+---The array will specifify the eight chars building up the border in a clockwise fashion starting with the top-left corner.
+---:h nvim_open_win
+---@field icons table @field separator fuzzy_prompt which_prompt
+---@field input_win_row_offset number The width for shift up output-window's row with input-window's height.
+---@width number a width for windows
+
 ---@class WFOptions
+---@field title string?
+---@field selector "which"|"fuzzy"
+---@field text_insert_in_advance string
+---@field key_group_dict table
+---@field prefix_size number
+---@field sorter function
+---@field behavior WFBehavior
+---@field output_obj_which_mode_desc_format fun(match_obj:Cell[]):Chunks
+---@field style WFStyle
